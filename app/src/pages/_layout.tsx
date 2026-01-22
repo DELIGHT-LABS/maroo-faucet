69481a04508ed1870720e88007bb53d6b17a4951
Wonhee Lee <2wheeh@gmail.com>
2026-01-22 19:46:48 +0900

    fix: correct fonts weight

--- app/src/pages/_layout.tsx
+++ app/src/pages/_layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
         rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
         precedence="font"
       />
 
