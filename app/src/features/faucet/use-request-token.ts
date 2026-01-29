5db622a5348fb1cbc2e140462a075ce2804f137e
Wonhee Lee <2wheeh@gmail.com>
2026-01-29 18:15:29 +0900

    feat: enable recaptcha when key is provided only

--- app/src/features/faucet/use-request-token.ts
+++ app/src/features/faucet/use-request-token.ts
@@ -1,17 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
+import { RECAPTCHA_SITE_KEY } from "~/shared/lib/env";
+
 import { requestTokens } from "./api";
 
 export const useRequestToken = () => {
   const { executeRecaptcha } = useGoogleReCaptcha();
   return useMutation({
     mutationFn: async (params: { address: string }) => {
-      if (!executeRecaptcha) {
+      if (!!RECAPTCHA_SITE_KEY && !executeRecaptcha) {
         throw new Error("ReCaptcha not loaded");
       }
 
-      const token = await executeRecaptcha("faucet_request");
+      // TODO: separate recaptcha action to common
+      const token = (await executeRecaptcha?.("faucetdrip")) ?? "";
 
       return requestTokens({
         address: params.address,
