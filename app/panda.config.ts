16461be431bd0e9bff1d5d71ab367221b0dff0b0
Wonhee Lee <2wheeh@gmail.com>
2026-02-03 00:20:12 +0900

    fix: correct hover style on mobile device

--- app/panda.config.ts
+++ app/panda.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  // Custom conditions
+  conditions: {
+    extend: {
+      supportHover: ["@media (hover: hover) and (pointer: fine)", "&:hover"],
+    },
+  },
+
   // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}"],
 
