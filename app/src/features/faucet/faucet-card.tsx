fa7d52d866b8d98ff0c78f45d881a3fc08a71b5f
Wonhee Lee <2wheeh@gmail.com>
2026-02-03 17:27:59 +0900

    refactor: extract StepCard for reusability

--- app/src/features/faucet/faucet-card.tsx
+++ app/src/features/faucet/faucet-card.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import { css } from "styled-system/css";
-import { center, divider, flex } from "styled-system/patterns";
+import { center } from "styled-system/patterns";
 import { useAccount, useWaitForTransactionReceipt } from "wagmi";
 
 import { AddNetworkButton } from "~/features/wallet/add-network-button";
@@ -14,14 +14,22 @@ import IconSpinner from "~/shared/assets/icon-spinner.svg?react";
 import { marooTestnet } from "~/shared/lib/chain";
 import { Button } from "~/shared/ui/button";
 import { ErrorCard } from "~/shared/ui/error-card";
-import { Idx } from "~/shared/ui/idx";
+import { StepCard, StepCards } from "~/shared/ui/step-card";
 
 import { isRateLimitError } from "./error";
 import { useRequestToken } from "./use-request-token";
 import { useTimer } from "./use-timer";
 
-export function FaucetCard() {
-  const { address } = useAccount();
+const ConnectStep = () => (
+  <StepCard active idx={1} title="Connect your wallet">
+    <ConnectButton />
+
+    <AddNetworkButton />
+  </StepCard>
+);
+
+const RequestStep = () => {
+  const { address, chainId, isDisconnected } = useAccount();
   const {
     mutate: requestToken,
     data,
@@ -36,7 +44,6 @@ export function FaucetCard() {
 
   const isRateLimited = isRateLimitError(error);
 
-  const { chainId, isDisconnected } = useAccount();
   const step = chainId === marooTestnet.id ? 2 : 1;
 
   useEffect(() => {
@@ -48,115 +55,77 @@ export function FaucetCard() {
   const { m, s, isTicking } = useTimer(isRateLimited ? error.retryAfter : 0);
 
   return (
-    <div
-      className={css({
-        border: "1px solid",
-        borderColor: "primary",
-        bg: "white",
-        rounded: "16px",
-        w: { base: "full", md: "500px" },
-        mb: "16px",
-        p: { base: "24px", md: "32px" },
-      })}
+    <StepCard
+      idx={2}
+      title="Request test tokens to your wallet"
+      active={step === 2}
     >
-      <div className={flex({ flexDir: "column" })}>
-        <div
-          className={flex({ gap: "16px", mb: "16px", alignItems: "center" })}
+      {!error && address && (
+        <Button
+          onClick={() => requestToken({ address })}
+          type="button"
+          disabled={isLoading || isConfirmed}
+          className={center({ gap: "8px" })}
         >
-          <Idx i={1} />
-          <p className={css({ textStyle: "body1.bold", color: "gray.900" })}>
-            Connect your wallet
-          </p>
+          {isLoading && (
+            <>
+              Requesting...
+              <IconSpinner className={css({ animation: "spin" })} />
+            </>
+          )}
+          {isConfirmed && (
+            <>
+              Tokens Sent!
+              <IconCheck />
+            </>
+          )}
+          {!isLoading && !isConfirmed && "Request Tokens"}
+        </Button>
+      )}
+      {isConfirmed && address && (
+        <Button
+          onClick={reset}
+          color="transparent"
+          type="button"
+          className={center({ gap: "8px", mt: "16px" })}
+        >
+          <IconRefresh />
+          Request More
+        </Button>
+      )}
+      {error && (
+        <div className={css({ w: 0, minW: "full" })}>
+          <ErrorCard message={error.message} />
+          {isTicking ? (
+            <div
+              className={center({
+                h: "40px",
+                mt: "16px",
+                color: "gray.500",
+                bg: "gray.100",
+                opacity: 0.5,
+                rounded: "8px",
+                textStyle: "button.m",
+              })}
+            >
+              Try again in {m}:{s}
+            </div>
+          ) : (
+            <Button
+              color="transparent"
+              onClick={reset}
+              className={center({ gap: "8px", mt: "16px" })}
+            >
+              <IconRefresh />
+              Retry
+            </Button>
+          )}
         </div>
-        <ConnectButton />
-
-        <AddNetworkButton />
-      </div>
-
-      <div
-        className={divider({
-          mt: { base: "20px", md: "32px" },
-          mb: { base: "20px", md: "32px" },
-          color: "gray.200",
-        })}
-      />
-
-      <div className={flex({ flexDir: "column", gap: "16px" })}>
-        <div className={flex({ gap: "16px", alignItems: "center" })}>
-          <Idx i={2} color={step === 2 ? "primary" : "ghost"} />
-          <p
-            className={css({
-              textStyle: "body1.bold",
-              color: step === 2 ? "gray.900" : "gray.400",
-            })}
-          >
-            Request test tokens to your wallet
-          </p>
-        </div>
-
-        {step === 2 && !error && address && (
-          <Button
-            onClick={() => requestToken({ address })}
-            type="button"
-            disabled={isLoading || isConfirmed}
-            className={center({ gap: "8px" })}
-          >
-            {isLoading && (
-              <>
-                Requesting...
-                <IconSpinner className={css({ animation: "spin" })} />
-              </>
-            )}
-            {isConfirmed && (
-              <>
-                Tokens Sent!
-                <IconCheck />
-              </>
-            )}
-            {!isLoading && !isConfirmed && "Request Tokens"}
-          </Button>
-        )}
-        {step === 2 && isConfirmed && address && (
-          <Button
-            onClick={reset}
-            color="transparent"
-            type="button"
-            className={center({ gap: "8px" })}
-          >
-            <IconRefresh />
-            Request More
-          </Button>
-        )}
-        {step === 2 && error && (
-          <div className={css({ w: 0, minW: "full" })}>
-            <ErrorCard message={error.message} />
-            {isTicking ? (
-              <div
-                className={center({
-                  h: "40px",
-                  mt: "16px",
-                  color: "gray.500",
-                  bg: "gray.100",
-                  opacity: 0.5,
-                  rounded: "8px",
-                  textStyle: "button.m",
-                })}
-              >
-                Try again in {m}:{s}
-              </div>
-            ) : (
-              <Button
-                color="transparent"
-                onClick={reset}
-                className={center({ gap: "8px", mt: "16px" })}
-              >
-                <IconRefresh />
-                Retry
-              </Button>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
+      )}
+    </StepCard>
   );
+};
+
+export function FaucetCard() {
+  return <StepCards StepOne={<ConnectStep />} StepTwo={<RequestStep />} />;
 }
